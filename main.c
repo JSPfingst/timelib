448186f96492326f884d0be628a9301e2f10f759
Jonas Pfingstgit <jspfingst@gmx.de>
2018-12-18 10:23:57 +0100

    Included isLeapYearfunction

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 /**
- *
- *
+ * Jonas Pfingst
+ *  18.12.18
  *
  **/
 
@@ -11,6 +11,7 @@
  *
  *
  **/
+int isLeapyear(int year);
 int day_of_the_year(int day, int month, int year)
 {
 
@@ -21,4 +22,31 @@ int main()
 {
     printf("Tag des Jahres: %i\n", day_of_the_year(31, 12, 2018));
     return 0;
+}
+int isLeapyear(int year)
+{
+ if(year>=1582)
+ {
+   if(year%4 ==0) {                                                //Jahr ist durch 4 teilbar
+
+        if(year%100 ==0) {                                          //Jahr ist durch 100 teilbar
+
+            if(year%400 ==0) {                                      //Jahr ist durch 400 teilbar
+                return 1;
+                             }
+            else {
+                return 0;
+                 }
+                         }
+        else {
+           return 1;
+             }
+                   }
+    else {
+        return 0;
+         }
+ }
+ else {
+    return -1;
+      }
 }
