f6948c17742f4fb883e10b8dac0b641291975f4d
JSPfingst <jspfingst@gmx.de>
2019-01-15 11:14:53 +0100

    Modified isleapyear Added weekdays startingModified isleapyear Added weekdays

--- main.c
+++ main.c
@@ -16,15 +16,16 @@ int input_date();
 int get_days_for_month(int month,int year);
 int exits_date(int day,int month,int year);
 int day_of_the_year(int day, int month, int year);
-
+void weekdays(int day,int month, int year);
 
 
 
 
 int main()
 {
-    int a = day_of_the_year(10,3,2018);
+    int a = day_of_the_year(7,1,1582);
     printf("%i",a);
+    weekdays(7,1,1582);
     //printf("Tag des Jahres: %i\n", day_of_the_year(31, 12, 2018));
     return 0;
 }
@@ -36,36 +37,27 @@ ob es sich bei dem eigegeben Jahr um ein schaltjahr handelt.
 int is_leapyear(int year){
  if(year>=1582)
  {
-   if(year%4 ==0) {                                                //Jahr ist durch 4 teilbar
-
-        if(year%100 ==0) {                                          //Jahr ist durch 100 teilbar
-
-            if(year%400 ==0) {                                      //Jahr ist durch 400 teilbar
-                return 1;
-                             }
-            else {
-                return 0;
-                 }
-                         }
-        else {
-           return 1;
-             }
-                   }
-    else {
-        return 0;
-         }
+   if(year%400 == 0) {
+    return 1;
+   }
+   else if(year%100 == 0) {
+    return 0;
+   }
+   else if(year%4 == 0) {
+    return 1;
+   }
+   else {
+    return 0;
+   }
  }
  else {
     return -1;
-      }
+ }
 }
 /**
 * Funktion ermittelt die Tage die der Monat im jeweiligen Jahr hat.
 **/
 int get_days_for_month(int month, int year) {
-if (is_leapyear(year)== 1 && month == 2) {                      // Entscheidung ob es sich um ein Schaltjhr handelt
-    month = 13;
-    }
 switch(month) {
 case 1:
 case 3:
@@ -113,6 +105,38 @@ int day_of_the_year(int day, int month, int year) {
         days += get_days_for_month(i,year); }
     days += day;                                        //Addition der im laufenden Monat vergangenen Tage
     return days;
+}
+
+/**
+Funktion zur Berechnung des Wochentages anhand des Datums
+**/
+void weekdays(int day,int month, int year) {
+int days = 0;
+int weekday = 0;
+//Wahl ob Schaltjahr oder nicht
+for(int i = 1582;i<year;i++) {
+    if (is_leapyear(year)==1) {
+        days += 366;
+    }
+    else {
+        days += 365;
+    }
+}
+//Addition der vergangenen Tage
+    days += day_of_the_year(day,month,year);
+//Bestimmen des Wochentages aufgrund der Differenz zum 1.1.1582
+    weekday = days%7;
+//Ausgabe des Wochentages
+    switch (weekday) {
+    case 0: printf("Der Tag ist ein Donnerstag");    break;
+    case 1: printf("Der Tag ist ein Freitag");    break;
+    case 2: printf("Der Tag ist ein Samstag");    break;
+    case 3: printf("Der Tag ist ein Sonntag");     break;
+    case 4: printf("Der Tag ist ein Montag");   break;
+    case 5: printf("Der Tag ist ein Dienstag");   break;
+    case 6: printf("Der Tag ist ein Mittwoch"); break;
+    }
+
 }
 
 
